5b179457518bbf7fabc2392f3385bf05c101ede6
Dmitry <smartass@protonmail.ch>
2017-11-02 07:23:54 +0300

    empty

---
+++ inv/free/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace free
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.investCount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.num = new System.Windows.Forms.NumericUpDown();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.NumericUpDown();
+            this.txt2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMoney = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.контекстноеМенюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оРазработчикахToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMoney)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.investCount});
+            this.grid.Location = new System.Drawing.Point(8, 27);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(643, 251);
+            this.grid.TabIndex = 0;
+            this.grid.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.grid_EditingControlShowing);
+            this.grid.Sorted += new System.EventHandler(this.grid_Sorted);
+            // 
+            // investCount
+            // 
+            this.investCount.HeaderText = "Вкладываемые средства";
+            this.investCount.Name = "investCount";
+            this.investCount.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGen.Location = new System.Drawing.Point(182, 361);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(193, 51);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "Заполнить таблицу";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(381, 352);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(270, 62);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Выполнить ";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество предприятий:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество вариантов вложения:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Предел генерирования:";
+            // 
+            // num
+            // 
+            this.num.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.num.Location = new System.Drawing.Point(9, 392);
+            this.num.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.num.Name = "num";
+            this.num.Size = new System.Drawing.Size(155, 20);
+            this.num.TabIndex = 9;
+            this.num.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(381, 286);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(270, 60);
+            this.btnOk.TabIndex = 10;
+            this.btnOk.Text = "Построить таблицу";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt1.Location = new System.Drawing.Point(182, 309);
+            this.txt1.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.txt1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(193, 20);
+            this.txt1.TabIndex = 11;
+            this.txt1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // txt2
+            // 
+            this.txt2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txt2.Location = new System.Drawing.Point(182, 335);
+            this.txt2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(193, 20);
+            this.txt2.TabIndex = 12;
+            this.txt2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Сумма вложения:";
+            // 
+            // txtMoney
+            // 
+            this.txtMoney.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtMoney.Location = new System.Drawing.Point(182, 284);
+            this.txtMoney.Maximum = new decimal(new int[] {
+            99999999,
+            0,
+            0,
+            0});
+            this.txtMoney.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtMoney.Name = "txtMoney";
+            this.txtMoney.Size = new System.Drawing.Size(193, 20);
+            this.txtMoney.TabIndex = 14;
+            this.txtMoney.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.контекстноеМенюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(663, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // контекстноеМенюToolStripMenuItem
+            // 
+            this.контекстноеМенюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem,
+            this.оРазработчикахToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.контекстноеМенюToolStripMenuItem.Name = "контекстноеМенюToolStripMenuItem";
+            this.контекстноеМенюToolStripMenuItem.Size = new System.Drawing.Size(123, 20);
+            this.контекстноеМенюToolStripMenuItem.Text = "Контекстное меню";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click_1);
+            // 
+            // оРазработчикахToolStripMenuItem
+            // 
+            this.оРазработчикахToolStripMenuItem.Name = "оРазработчикахToolStripMenuItem";
+            this.оРазработчикахToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.оРазработчикахToolStripMenuItem.Text = "Разработчики";
+            this.оРазработчикахToolStripMenuItem.Click += new System.EventHandler(this.оРазработчикахToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 430);
+            this.Controls.Add(this.txtMoney);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.num);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "  Решение задачи распределения капиталовложений";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMoney)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown num;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.NumericUpDown txt1;
+        private System.Windows.Forms.NumericUpDown txt2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn investCount;
+        private System.Windows.Forms.NumericUpDown txtMoney;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem контекстноеМенюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оРазработчикахToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ inv/free/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace free
+{
+    public partial class Form1 : Form
+    {
+        int factCount;
+        int varCount;
+        int MoneyCount;
+        string[] plan;
+        string[] newPlan;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            grid.RowHeadersVisible = false;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+            {
+                if (txt1.Value > 1)
+                { 
+                btnStart.Enabled = true;
+                    btnGen.Enabled = true;
+                        num.Enabled = true;
+                }
+                else
+                {
+                btnStart.Enabled = false;
+                    btnGen.Enabled = false;
+                        num.Enabled = false;
+                MessageBox.Show("Для выполнения расчетов количество предприятий, должно быть больше 1");
+            }
+        }
+            int moneyPart = 0;
+            try
+            {
+                factCount = Convert.ToInt32(txt1.Value);
+                varCount = (Convert.ToInt32(txt2.Value)+1);
+
+                if (txtMoney.Text == "")
+                {
+                    moneyPart = 1;
+                }
+                else
+                {
+                    MoneyCount = Convert.ToInt32(txtMoney.Text);
+                    moneyPart = MoneyCount / (varCount - 1);
+                }
+
+                grid.Rows.Clear();
+                grid.Columns.Clear();
+
+                DataGridViewColumn col1 = new DataGridViewTextBoxColumn();
+                col1.HeaderText = "Вкладываемые средства";
+                grid.Columns.Add(col1);
+
+                for (int i = 1; i < factCount + 1; i++)
+                {
+                    DataGridViewColumn col = new DataGridViewTextBoxColumn();
+                    col.HeaderText = "Fact" + i.ToString();
+                    grid.Columns.Add(col);
+                }
+
+                for (int i = 0; i < varCount; i++)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    grid.Rows.Add(row);
+                    grid.Rows[i].Cells[0].Value = (i*moneyPart).ToString();
+                }
+            }
+            catch (FormatException ex) { System.Console.WriteLine(ex.Data.ToString()); }
+            foreach (DataGridViewColumn column in grid.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+             Random random = new Random();
+             int[] mass;
+             for (int i = 1; i < grid.Columns.Count; i++)
+             {
+                 mass = new int[grid.Rows.Count];
+                 for (int j = 0; j < grid.Rows.Count; j++)
+                 {
+                     mass[j] = random.Next(0, (int)num.Value+1);
+                 }
+                 Array.Sort(mass);
+                 for (int j = 0; j < grid.Rows.Count; j++)
+                {
+                    if (Convert.ToInt32(grid.Rows[j].Cells[0].Value) == 0)
+                    {
+                        grid.Rows[j].Cells[i].Value = 0;
+
+                    }
+                    else grid.Rows[j].Cells[i].Value = mass[j].ToString();
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {          
+            try
+            {
+                int[] X1 = new int[varCount];//Варианты вложений
+                int[] Fn = new int[varCount];//выгодные функции из предыдущего шага
+                int[] fn = new int[varCount];//функции для предприятий из начальной таблицы
+                int[,] plus = new int[varCount, varCount];
+                plan = new string[varCount];
+                newPlan = new string[varCount];
+                for (int i = 0; i < X1.Length; i++)//запись вариантов вложений
+                {
+                    X1[i] = Convert.ToInt32(grid.Rows[i].Cells[0].Value);
+                }
+
+                for (int i = 0; i < varCount; i++)//запись начального Fn
+                {
+                    Fn[i] = Convert.ToInt32(grid.Rows[i].Cells[1].Value);
+                }
+
+
+
+                for (int fact = 0; fact < factCount - 1; fact++)//итерации вычислениый
+                {
+
+                    for (int i = 0; i < varCount; i++)//запись начального fn
+                    {
+                        fn[i] = Convert.ToInt32(grid.Rows[i].Cells[fact + 2].Value);
+                    }
+                    for (int i = 0; i < varCount; i++)//сложение элементов на пересечении 
+                    {
+
+                        for (int j = 0; j < varCount; j++)
+                        {
+                            if (X1[varCount - 1] >= (X1[i] + X1[j])) plus[i, j] = fn[i] + Fn[j];
+                        }
+                    }
+                    Array.Copy(newPlan, plan, plan.Length);
+                    for (int var = 0; var < varCount; var++)
+                    {
+                        int variant = X1[var];
+                        int max = 0;
+                        int x = 0, y = 0;
+
+                        for (int i = 0; i < varCount; i++)
+                        {
+                            for (int j = 0; j < varCount; j++)
+                                if (X1[j] + X1[i] == variant && plus[i, j] > max)
+                                {
+                                    max = plus[i, j];
+                                    x = i;
+                                    y = j;
+                                }
+
+                        }
+                        Fn[var] = plus[x, y];
+                        if (fact == 0)
+                        {
+                            newPlan[var] = X1[y].ToString() + " " + X1[x].ToString();
+                        }
+                        else
+                        {
+                            newPlan[var] = plan[y] + " " + X1[x].ToString();
+                        }
+                    }
+
+
+                }
+
+                int max1 = 0;
+                int x1 = 0;
+                string res = "";
+                for (int i = 0; i < varCount; i++)
+                {
+                    if (Fn[i] > max1)
+                    {
+                        max1 = Fn[i];
+                        x1 = i;
+                    }
+                }
+                int kolRes=0;
+                for (int i = 0; i < grid.RowCount; i++)
+                    if (Convert.ToInt32(grid.Rows[i].Cells[0].Value) != 0) kolRes++;
+                res = "Наибольший прирост, при " + Convert.ToString(kolRes) +  " вариантах вложения между " + factCount.ToString();
+                if (factCount <= 4)
+                {
+                   res+= " -мя предприятиями, составит " + Fn[x1].ToString() + "\n";
+                }
+                else
+                    res+= " -ю предприятиями, составит " + Fn[x1].ToString() + "\n";
+                for (int i = 0; i < factCount; i++)
+                {
+                    res += (i + 1).ToString() + "-му предприятию выделить " + newPlan[x1].Split(' ')[i] + "\n";
+                    //res += "вложить в " + (i + 1).ToString() + " предприятие " + newPlan[x1].Split(' ')[i] + "\n";
+                }
+                MessageBox.Show(res, "Ответ", MessageBoxButtons.OK);
+
+            }
+            catch (Exception) { }
+        }
+
+        private void grid_Sorted(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show(
+             "Вы действительно хотите выйти из программы?",
+             "Завершение программы",
+             MessageBoxButtons.YesNo,
+             MessageBoxIcon.Warning
+            );
+            if (dialog == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void grid_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+
+            if (grid.CurrentCell.ColumnIndex >= 0)
+            {
+                TextBox tb = (TextBox)e.Control;
+                tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+            }
+            else
+            {
+                TextBox tb = (TextBox)e.Control;
+                tb.KeyPress -= tb_KeyPress;
+            }
+        }
+
+        void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!Char.IsNumber(e.KeyChar) && (e.KeyChar != '-') && (e.KeyChar != ',')))
+            {
+                if ((e.KeyChar != (char)Keys.Back) || (e.KeyChar != (char)Keys.Delete))
+                { e.Handled = true; }
+            }
+
+        }
+
+        private void справкаToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("Help.chm");
+        }
+
+        private void выходToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оРазработчикахToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Студенты 346 группы, специальности 230115 Чернышов Дмитрий и Миннеханов Ильназ");
+        }
+    }
+}
+
+    
